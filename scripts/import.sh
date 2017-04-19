943fffca0e0fc1e77ee437c986cd6421a76f103a
Dmitry Shulyak <yashulyak@gmail.com>
2017-04-19 13:27:59 +0300

    Implement e2e testing for rudder and helm

---
+++ scripts/import.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+IMAGE_REPO=${IMAGE_REPO:-mirantis/rudder-appcontroller}
+IMAGE_TAG=${IMAGE_TAG:-latest}
+NUM_NODES=${NUM_NODES:-2}
+TMP_IMAGE_PATH=${TMP_IMAGE_PATH:-/tmp/image.tar}
+MASTER_NAME=${MASTER_NAME="kube-master"}
+SLAVE_PATTERN=${SLAVE_PATTERN:-"kube-node-"}
+
+
+function import-image {
+    docker save ${IMAGE_REPO}:${IMAGE_TAG} -o "${TMP_IMAGE_PATH}"
+
+    if [ -n "$MASTER_NAME" ]; then
+        docker cp "${TMP_IMAGE_PATH}" kube-master:/image.tar
+        docker exec -ti "${MASTER_NAME}" docker load -i /image.tar
+    fi
+
+    for i in `seq 1 "${NUM_NODES}"`;
+    do
+        docker cp "${TMP_IMAGE_PATH}" "${SLAVE_PATTERN}$i":/image.tar
+        docker exec -ti "${SLAVE_PATTERN}$i" docker load -i /image.tar
+    done
+    set +o xtrace
+    echo "Finished copying docker image to dind nodes"
+}
+
+import-image
---
+++ test/e2e_test.go
@@ -0,0 +1,63 @@
+// Copyright 2017 Mirantis
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
+)
+
+var _ = Describe("Basic Suite", func() {
+
+	var helm HelmManager
+	var clientset kubernetes.Interface
+
+	BeforeEach(func() {
+		var err error
+		clientset, err = KubeClient()
+		Expect(err).NotTo(HaveOccurred())
+		By("Creating namespace and initializing test framework")
+		namespaceObj := &v1.Namespace{
+			ObjectMeta: v1.ObjectMeta{
+				GenerateName: "e2e-appcontroller-rudder-",
+			},
+		}
+		namespace, err := clientset.Namespaces().Create(namespaceObj)
+		Expect(err).NotTo(HaveOccurred())
+		helm = &BinaryHelmManager{Namespace: namespace}
+		By("Installing helm")
+		Expect(helm.InstallHelm("")).NotTo(HaveOccurred())
+		By("Installing tiller")
+		Expect(helm.InstallTiller()).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		By("Deleting tiller")
+		Expect(helm.DeleteTiller(false)).NotTo(HaveOccurred())
+		By("Removing namespace")
+		DeleteNS(clientset, helm.Namespace)
+	})
+
+	It("Should be possible to create/delete/update and check status of wordpress chart", func() {
+		By("Install chart stable/wordpress")
+		releaseName, err := helm.Install("stable/wordpress")
+		Expect(err).NotTo(HaveOccurred())
+		By("Check status of release " + releaseName)
+		Expect(helm.Status(releaseName)).NotTo(HaveOccurred())
+	})
+})
